57e363ac60fed882b6b2dd4e6ac05b5046d1ac00
goztrk <me@gokhan.org.tr>
2023-07-01 17:44:41 +0300

    Add protocol config value

--- zion/conf.py
+++ zion/conf.py
@@ -10,6 +10,7 @@ from django.conf import settings  # noqa
 class ZionAppConf(AppConf):
     TEMPLATE_CONTEXT = "zion.views.contexts.template_context"
     SITE_NAME = "Zion"
+    DEFAULT_HTTP_PROTOCOL = "http"
 
     class Meta:
         prefix = "zion"
