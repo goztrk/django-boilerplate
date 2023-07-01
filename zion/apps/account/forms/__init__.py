3299411ae1995b6b43898702606e8b53bb6e4568
goztrk <me@gokhan.org.tr>
2023-07-01 20:31:55 +0300

    Add Login Forms

---
+++ zion/apps/account/forms/__init__.py
@@ -0,0 +1,11 @@
+# ZION Shared Library Imports
+from zion.apps.account.forms.login import (
+    LoginEmailForm,
+    LoginUsernameForm,
+)
+
+
+__all__ = [
+    "LoginUsernameForm",
+    "LoginEmailForm",
+]
