2981e8f7805305727d2c5162a8b0a2273d3de628
goztrk <me@gokhan.org.tr>
2023-07-01 17:44:01 +0300

    Update Account:create method

--- zion/apps/account/models/account.py
+++ zion/apps/account/models/account.py
@@ -7,9 +7,33 @@ from django.db import (
 from zion.apps.account.conf import (
     settings,
 )
+from zion.apps.account.models.email import (
+    EmailAddress,
+)
 
 
 class Account(models.Model):
     user = models.OneToOneField(
         settings.AUTH_USER_MODEL, related_name="account", on_delete=models.CASCADE
     )
+
+    class Meta:
+        app_label = "account"
+        verbose_name = "Account"
+
+    def __str__(self):
+        return str(self.user)
+
+    @classmethod
+    def create(cls, request=None, **kwargs):
+        create_email = kwargs.pop("create_email", True)
+        confirm_email = kwargs.pop("confirm_email", None)
+
+        account = cls(**kwargs)
+        account.save()
+        if create_email and account.user.email:
+            kwargs = {"primary": True}
+            if confirm_email is not None:
+                kwargs["confirm"] = confirm_email
+            EmailAddress.objects.add_email(account.user, account.user.email, **kwargs)
+        return account
