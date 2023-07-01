c2798a42783d93a61326dd97c4a69cad738dec30
goztrk <me@gokhan.org.tr>
2023-07-01 20:32:08 +0300

    Add Hooks

--- zion/apps/account/hooks.py
+++ zion/apps/account/hooks.py
@@ -7,6 +7,12 @@ class AccountDefaultHooks(ZionBaseHooks):
     def generate_signup_code_token(self, email=None):
         return self.generate_random_token([email])
 
+    def get_user_credentials(self, form, identifier_field):
+        return {
+            "username": form.cleaned_data[identifier_field],
+            "password": form.cleaned_data["password"],
+        }
+
 
 class HookProxy(object):
     def __getattr__(self, attr):
