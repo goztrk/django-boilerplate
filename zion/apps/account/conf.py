653fedb8537f4907198a3821b4c7195aa3f4f929
goztrk <me@gokhan.org.tr>
2023-07-01 22:54:01 +0300

    Add more settings for account app

--- zion/apps/account/conf.py
+++ zion/apps/account/conf.py
@@ -9,20 +9,32 @@ from zion.constants.timezones import TIMEZONES
 
 
 class AccountAppConf(AppConf):
-    LOGIN_FIELD = "username"
     CREATE_ON_SAVE = True
+
+    LOGIN_FIELD = "username"
+    REMEMBER_ME_EXPIRY = 60 * 60 * 24 * 365 * 10
+
     LOGIN_URL = "account:login"
     LOGOUT_URL = "account:logout"
     SIGNUP_REDIRECT_URL = "/"
     LOGIN_REDIRECT_URL = "/"
     LOGOUT_REDIRECT_URL = "/"
+    PASSWORD_CHANGE_REDIRECT_URL = "account:password"
+    PASSWORD_RESET_REDIRECT_URL = "account:login"
+    PASSWORD_RESET_TOKEN_URL = "account:password_reset_token"
+    EMAIL_CONFIRMATION_URL = "account:confirm_email"
+    EMAIL_CONFIRMATION_ANONYMOUS_REDIRECT_URL = "account:login"
+    SETTINGS_REDIRECT_URL = "account:settings"
+
+    PASSWORD_EXPIRY = 0
+    PASSWORD_USE_HISTORY = False
     PASSWORD_STRIP = True
-    REMEMBER_ME_EXPIRY = 60 * 60 * 24 * 365 * 10
+    NOTIFY_ON_PASSWORD_CHANGE = True
+
     EMAIL_CONFIRMATION_EXPIRE_DAYS = 3
     EMAIL_CONFIRMATION_AUTO_LOGIN = False
-    EMAIL_CONFIRMATION_ANONYMOUS_REDIRECT_URL = "account:login"
     EMAIL_CONFIRMATION_AUTHENTICATED_REDIRECT_URL = None
-    EMAIL_CONFIRMATION_URL = "account:confirm_email"
+
     HOOKS = "zion.apps.account.hooks.AccountDefaultHooks"
     TIMEZONES = TIMEZONES
 
