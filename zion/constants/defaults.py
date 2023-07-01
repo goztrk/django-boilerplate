054b4d0004e0152dc06452bad9d22a9e925dd141
goztrk <me@gokhan.org.tr>
2023-07-01 10:48:49 +0300

    Add site name constant to template context

--- zion/constants/defaults.py
+++ zion/constants/defaults.py
@@ -1,1 +1,3 @@
 ZION_TEMPLATE_CONTEXT = "zion.views.contexts.template_context"
+
+ZION_SITE_NAME = "Zion"
--- zion/views/contexts.py
+++ zion/views/contexts.py
@@ -3,7 +3,13 @@ from socket import (
     gethostname,
 )
 
+# ZION Shared Library Imports
+from zion.utils import (
+    zion_setting,
+)
+
 
 def template_context(request, context={}):
+    context["site_name"] = zion_setting("ZION_SITE_NAME")
     context["server"] = {"hostname": gethostname()}
     return context
