196ec0de57b4482cc48a0d2c2d6063aa212da745
goztrk <me@gokhan.org.tr>
2023-07-01 23:24:13 +0300

    Add sign up view

--- zion/apps/account/views/__init__.py
+++ zion/apps/account/views/__init__.py
@@ -2,10 +2,12 @@
 from zion.apps.account.views.confirm_email import ConfirmEmailView
 from zion.apps.account.views.login import LoginView
 from zion.apps.account.views.logout import LogoutView
+from zion.apps.account.views.signup import SignupView
 
 
 __all__ = [
     "ConfirmEmailView",
     "LoginView",
     "LogoutView",
+    "SignupView",
 ]
