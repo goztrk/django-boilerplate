df6b1d0b14169f9fb79f958d2795946df659bd85
goztrk <me@gokhan.org.tr>
2023-07-01 17:44:24 +0300

    Add config values

--- zion/apps/account/conf.py
+++ zion/apps/account/conf.py
@@ -8,5 +8,10 @@ from django.conf import settings  # noqa
 
 
 class AccountAppConf(AppConf):
+    CREATE_ON_SAVE = True
+    EMAIL_CONFIRMATION_EXPIRE_DAYS = 3
+    EMAIL_CONFIRMATION_URL = "account:confirm_email"
+    HOOKS = "zion.apps.account.hooks.AccountDefaultHooks"
+
     class Meta:
         prefix = "zion_account"
