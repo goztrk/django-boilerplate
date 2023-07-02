ccc8047e4ac16c03fea3a623e27e6ecccee44910
goztrk <me@gokhan.org.tr>
2023-07-02 11:38:04 +0300

    Fix static file serving with `runserver` in dev

--- core/settings/environments/development.py
+++ core/settings/environments/development.py
@@ -1,7 +1,11 @@
 """
 Django Settings Environment Override
 """
+# Python Standard Library
+import os
+
 # Application Imports
+from core.settings.components.dirs import BASE_DIR
 from core.settings.components.templates import TEMPLATES
 
 
@@ -9,3 +13,8 @@ DEBUG = True
 TEMPLATE_DEBUG = True
 
 TEMPLATES[0]["OPTIONS"]["debug"] = TEMPLATE_DEBUG
+
+# To make static file serving work with `runserver`, `STATICFILES_DIRS` must be
+# defined. But it does not work with `STATIC_ROOT`.
+STATIC_ROOT = ""
+STATICFILES_DIRS = [os.path.join(BASE_DIR.parent, "static")]
